2ae7f2233fd9f2a2b643d7493154abfd23443311
Logan <logan@logantalbot.com>
2024-03-21 22:48:43 +0000

    convert to toml & makes it executable

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-
 ## Useful link
 
 - https://python.langchain.com/docs/integrations/vectorstores/azuresearch
--
+- 
+
+
+# Useful Comments
+
+
+Generate Code Coverage
+
+```
+pytest tests --cov src  --cov-report html
+```
---
+++ src/document_importer/__main__.py
@@ -0,0 +1,27 @@
+# Project main file
+import os
+from icecream import ic
+from dotenv import load_dotenv, dotenv_values
+import logging
+from document_importer.importer import Importer
+
+def main() -> None:
+    """
+    Main function that imports documents, loads them into the vector search, and performs a similarity search.
+    """
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    
+    logging.info("Starting the document importer...")
+    logging.info("-----------------Starting Script-----------------")
+    # Load the environment variables
+    load_dotenv(override=True)
+    config = {
+        **dotenv_values(),
+    }
+    logging.info(f"Imported configuration of length: {len(config.keys())}")
+    Importer(config, repository="defra/example", directory="docs").run()
+
+    logging.info("-----------------Script Completed-----------------") 
+
+if __name__ == "__main__":
+    main()
